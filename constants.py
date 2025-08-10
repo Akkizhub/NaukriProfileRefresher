793e44f97cce460d72cb78f0c4a9f3ffdf039c0a
Akkizhub <akshayrock875@gmail.com>
2025-08-10 17:30:51 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ USERNAME = "akshayreddy.baddam@gmail.com"
 PASSWORD = "Akki@nkri29"
 MOBILE = "9666524777"
 USER_DATA_DIR = "naukri_profile"
+HEADLESS = True
 ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
 MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
 NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
